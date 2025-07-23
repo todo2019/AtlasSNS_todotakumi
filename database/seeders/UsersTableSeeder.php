8e1a45dceac5ec1067dd1c8b74be0c2b11c92834
DESKTOP-TP6RG45\todou <todou71093@gmail.com>
2025-07-23 20:05:37 +0900

    コミット

---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+use Illuminate\Support\Facades\DB;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        DB::table('users')->insert([
+          ['username'=>'Atlas一郎',
+          'mail'=>'user@example.com',
+          'password'=>bcrypt('LULL'),
+          'bio'=>'よろしくね',
+          'images'=>'']
+        ]);
+    }
+}
