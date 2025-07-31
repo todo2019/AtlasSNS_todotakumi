90aeac22f3a879bcac4c7e498c8a489f73c8968c
DESKTOP-TP6RG45\todou <todou71093@gmail.com>
2025-07-31 19:23:19 +0900

    プロフィールの項目だけ作ったよ

--- resources/views/profiles/profile.blade.php
+++ resources/views/profiles/profile.blade.php
@@ -1,5 +1,26 @@
 <x-login-layout>
 
+  <form action="/update" method="POST" enctype="multipart/form-data">
+    @csrf
 
+   <ul class="UserProfile">
+      <li>ユーザー名<input type="text" name="username" value="{{ Auth::user()->username }}"></li>
 
+      <li>メールアドレス<input type="text" name="mail" value="{{ Auth::user()->email }}"></li>
+
+      <li>パスワード<input type="password" name="password" value="{{ Auth::user()->password }}"></li>
+
+      <li>パスワード確認<input type="password" name="password_confirmation" value="{{ Auth::user()->password }}"></li>
+
+      <li>自己紹介<input type="text" name="bio" value="{{ Auth::user()->bio }}"></li>
+
+      <li>アイコン画像<input type="file" name="image"></li>
+
+    </ul>
+
+    <button type="submit" class="btn btn-success" >
+      {{ __('更新') }}
+    </button>
+
+</form>
 </x-login-layout>
