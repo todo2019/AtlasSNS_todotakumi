9785db4aee179ab90043eff02fb8f8c5cc50c4ef
DESKTOP-TP6RG45\todou <todou71093@gmail.com>
2025-08-09 15:53:19 +0900

    searchでのユーザ表示欄を自分以外にした

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 use App\Models\User;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
 
 class UsersController extends Controller
 {
@@ -13,7 +15,7 @@ class UsersController extends Controller
     }
 
     public function human(){
-        $users=User::get();
+        $users = User::where('id', '!=', Auth::id())->get();
         return view('users.search',['users'=>$users]);
     }
 }
